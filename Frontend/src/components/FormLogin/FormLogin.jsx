e65b71daaa3e3a75f1e1424592149bf78869205c
Volodymyr-2024 <itspec.ua@gmail.com>
2025-03-18 22:39:56 +0100

    Изменил команду npm run dev на npm run start

--- Frontend/src/components/FormLogin/FormLogin.jsx
+++ Frontend/src/components/FormLogin/FormLogin.jsx
@@ -31,9 +31,7 @@ function FormLogin(props) {
       localStorage.setItem("token", response.token);
       localStorage.setItem("userId", response.userId);
       setMessage("Login successfully!");
-      setTimeout(() => {
-        navigate("/main");
-      }, 1000);
+      setTimeout(() => navigate("/main"), 1000);
     } catch (error) {
       setMessage(
         error.response?.data?.message || error.message || "Login failed"
