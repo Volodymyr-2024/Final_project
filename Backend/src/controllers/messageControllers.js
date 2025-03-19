77b89ca8698ba519ed373a9c7b89740481dd5efb
Volodymyr-2024 <itspec.ua@gmail.com>
2025-03-19 20:14:44 +0100

    работа с сообщениями, изменил модель messageModel

--- Backend/src/controllers/messageControllers.js
+++ Backend/src/controllers/messageControllers.js
@@ -2,15 +2,15 @@ import Message from "../models/messageModel.js";
 import User from "../models/userModel.js";
 
 export const getMessages = async (req, res) => {
-  const userId = req.user.id;
+  const userId = req.userId;
   const { targetUserId } = req.params;
   try {
     const messages = await Message.find({
       $or: [
-        { sender_id: userId, receiver_id: targetUserId },
-        { sender_id: targetUserId, receiver_id: userId },
+        { senderId: userId, receiverId: targetUserId },
+        { senderId: targetUserId, receiverId: userId },
       ],
-    }).sort({ create_at: 1 });
+    }).sort({ createAt: 1 });
     res.status(200).json(messages);
   } catch (error) {
     res.status(500).json({ message: "Error receiving a message" });
@@ -18,9 +18,9 @@ export const getMessages = async (req, res) => {
 };
 
 export const sendMessage = async (req, res) => {
-  const userId = req.user.id;
+  const userId = req.userId;
   const { targetUserId } = req.params;
-  const { message_text } = req.body;
+  const { messageText } = req.body;
   try {
     const user = await User.findById(userId);
     const targetUser = await User.findById(targetUserId);
@@ -28,18 +28,18 @@ export const sendMessage = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
     const message = new Message({
-      sender_id: userId,
-      receiver_id: targetUserId,
-      message_text,
-      create_at: new Date(),
+      senderId: userId,
+      receiverId: targetUserId,
+      messageText,
+      createAt: new Date(),
     });
 
     await message.save();
 
     req.io.to(targetUserId).emit("receiveMessage", {
       senderId: userId,
-      messageText: message_text,
-      createdAt: message.created_at,
+      messageText: messageText,
+      createdAt: message.createAt,
     });
     res.status(201).json(message);
   } catch (error) {
